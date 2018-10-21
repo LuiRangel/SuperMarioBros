10cd56e9bb1b176638ae44145b02c76894a09391
LuiRangel <xxcmgxx02@gmail.com>
2018-10-20 19:03:55 -0700

    Menu

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 class Settings:
     def __init__(self):
         # screen settings
-        self.bg_color = (0, 0, 0)
+        self.bg_color = (130, 190, 245)
         self.screen_width = 1200
         self.screen_height = 800
 
