3b71aa2be322b74446c80b5e5841fa1f1b0feb6c
Alex Garcia <alex.garcia@csu.fullerton.edu>
2018-10-20 19:01:06 -0700

    Add files via upload

--- settings.py
+++ settings.py
@@ -2,8 +2,12 @@ class Settings:
     def __init__(self):
         # screen settings
         self.bg_color = (0, 0, 0)
-        self.screen_width = 1200
+        self.screen_width = 800
         self.screen_height = 800
 
         # game_active flag
         self.finished = False
+
+        #scoring
+        self.score = 0
+        self.high_score = 0
